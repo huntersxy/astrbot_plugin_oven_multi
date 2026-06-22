ed36ed1da154bab33790f5ea872f73cfe18c96af
huntersxy <1833505972@qq.com>
2026-06-22 19:49:21 +0800

    chore: 更新至 v1.8.1，修复指令消息被风格学习记录的问题
    - 跳过以 / 开头的指令消息，防止被 LLM 当作群聊风格学习

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ class ThinkingManager:
                     pass
 
 
-@register(PLUGIN_NAME, "汐兮雨", "插座的多功能烤箱", "1.8.0")
+@register(PLUGIN_NAME, "汐兮雨", "插座的多功能烤箱", "1.8.1")
 class OvenMultiPlugin(Star):
     def __init__(self, context: Context, config: AstrBotConfig = None):
         super().__init__(context)
